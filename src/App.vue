1c15e7df02bace552b5adbd269243d99a9b23de0
Nick Miloscia <nmiloscia@GMH-D25NH0HYF8JC.local>
2018-06-01 17:29:24 -0400

    Added array of quotes, added new quote textfield

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
     <div class="container">
-    
+    	<NewQuote @quoteAdded='newQuote'></NewQuote>
+    	<QuoteGrid :quotes='quotes'></QuoteGrid>
     </div>
 </template>
 
 <script>
+	import QuoteGrid from './components/QuoteGrid.vue';
+	import NewQuote from './components/NewQuote.vue';
     export default {
-        
+        data: function() {
+        	return {
+        		quotes: ['A famous quote'],
+        		maxQuotes: 10
+        	}
+        },
+        components: {
+        	QuoteGrid,
+        	NewQuote
+        },
+        methods: {
+        	newQuote: function(quote) {
+        		this.quotes.push(quote);
+        	}
+        }
     }
 </script>
 
---
+++ src/components/QuoteGrid.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="row">
+		<Quote v-for="quote in quotes">
+			{{quote}}
+		</Quote>
+	</div>
+</template>
+
+<script>
+	import Quote from './Quote.vue';
+	export default {
+		props: ['quotes'],
+		components: {
+			Quote
+		}
+	}
+</script>
+
+<style></style>
